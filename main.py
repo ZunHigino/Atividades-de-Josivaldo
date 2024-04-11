e19299942131e7a2aba2c27f67f37dfb166343eb
ZunHigino <anavcavalcantef@gmail.com>
2024-04-11 15:34:18 -0300

    Atividade 10

--- main.py
+++ main.py
@@ -1,31 +1,47 @@
-from banco import ContaBancaria
+from restaurante import Pedidos
 
 #atividade
 
 if __name__ == '__main__':
-    conta1 = ContaBancaria('123123', 1000, 'Higino')
-
-    print(type(conta1.saldo))
+    L_itens = []
+    L_total = []
+    pedindo = True
+    status = 1
     
-    rodar = True
+    while pedindo:
+        Pedidos.menu()
+        
+        pedido = int(input())
+        match pedido:
+            case 1:
+                item = 'Hamburguer'
+                total = 15
+                Pedidos.adicionar(L_itens, L_total, item,total)
+            case 2:
+                item = 'Pizza'
+                total = 25
+                Pedidos.adicionar(L_itens, L_total, item,total)
+            case 3:
+                item = 'Refrigerante'
+                total = 8
+                Pedidos.adicionar(L_itens, L_total, item,total) 
+            case 0:
+                pedindo = False
+            case _:
+                print('Pedido não identificado.')
+        
+        Pedidos.status(status)
+        
+    print(f'''Itens pedidos: {L_itens}
+Valor dos Itens: {L_total}''')
+    print(Pedidos.calculo_total(L_total))
+        
+    res = int(input('Comfirmar pedido? (1/0) '))
     
-    while rodar:
-        ContaBancaria.menu()
-
-        r = int(input())
-    
-        if r == 1:
-            qnt = int(input('Quanto dinheiro deseja sacar?: '))
-            conta1.sacar(qnt)
-                
-        elif r == 2:
-            qnt = int(input('Quanto dinheiro deseja depositar?: '))
-            conta1.depositar(qnt)
-            
-        elif r ==  3:
-            ContaBancaria.saldo_atual(conta1.saldo)
-            
-        elif r == 0:
-            rodar = False
-            
-    print('Obrigado pela visita!')
+    if res == 1:
+        print('Pagamento feito.')
+        status = 0
+        Pedidos.status(status)
+    elif res == 0:
+        status = 2
+        Pedidos.status(status)
